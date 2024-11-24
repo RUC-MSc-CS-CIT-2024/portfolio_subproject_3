833713799265cf78ab5fecf7a5dc340125fcd0cf
idax6797 <idahay24@gmail.com>
2024-11-24 21:15:51 +0100

    feat: setting up react project via vite with react  bootstrap, eslint, prettier

---
+++ src/frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button';
+
+export default function App() {
+  return (
+    <>
+      <h1>Movie Application</h1>
+      <Button variant="primary">Primary</Button>
+    </>
+  );
+}

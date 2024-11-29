732eb2a653063c05d177fca875f344f202c251d7
Sofus Hilfling <sofushn@hotmail.dk>
2024-11-29 08:18:34 +0100

    refactor: align route setup with reactrouter docs (#34)
    Aligns the route setup with examples in
    [docs](https://reactrouter.com/start/library/routing) and on the [react
    router
    github](https://github.com/remix-run/react-router/blob/v7_rsc/examples/auth/src/main.tsx).
    
    Basically removed array with routes and map function and moved
    `BrowserRouter` to `main.jsx` as shown on React Router's GitHub example.
    
    Additionally added index.js files for exports of each subfolder like
    `components` and `pages`

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,13 +1,28 @@
 import { AuthProvider } from './contexts/AuthContext';
-import { BrowserRouter } from 'react-router-dom';
-import Router from './pages/router';
+import { Route, Routes } from 'react-router-dom';
+import { Layout, PrivateRoute } from '@/components';
+import {
+  HomePage,
+  NotFoundPage,
+  ProfilePage,
+  ProfileSettingsPage,
+  SignInPage,
+} from '@/pages';
 
 export default function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="signin" element={<SignInPage />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="profile" element={<ProfilePage />} />
+            <Route path="profile-settings" element={<ProfileSettingsPage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
     </AuthProvider>
   );
 }

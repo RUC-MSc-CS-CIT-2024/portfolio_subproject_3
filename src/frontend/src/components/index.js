b1b5c896e3611521f586e9e77ec6c753f4065b6b
MarLaslo <168294377+MarLaslo@users.noreply.github.com>
2024-12-01 19:53:46 +0100

    feat:implementing-search-function (#48)
    Creating search on home page
    
    reusing SearchFrom component
    creating navigation hook
    passing searchquery in url
    making prop for a search button variant

--- src/frontend/src/components/index.js
+++ src/frontend/src/components/index.js
@@ -5,5 +5,6 @@ export { default as SearchForm } from './SearchForm/SearchForm';
 export { default as UpdateProfileForm } from './UpdateProfileForm/UpdateProfileForm';
 export { default as Layout } from './Layout';
 export { default as PrivateRoute } from './PrivateRoute';
+export { default as BackgroundContainer } from './BackgroundContainer/BackgroundContainer';
 export { default as ToastNotification } from './ToastNotification/ToastNotification';
 export { default as MovieCard } from './MovieCard/MovieCard';
--- src/frontend/src/pages/HomePage.jsx
+++ src/frontend/src/pages/HomePage.jsx
@@ -1,11 +1,23 @@
-import Container from 'react-bootstrap/Container';
+import { Container, Col, Row } from 'react-bootstrap';
+import { BackgroundContainer, SearchForm } from '../components';
 import { ToastNotification } from '@/components';
 
 export default function HomePage() {
   return (
-    <Container>
-      <h1>Homepage</h1>
-      <ToastNotification />
-    </Container>
+    <div>
+      <BackgroundContainer>
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center my-5 text-light">
+                Welcome to MovieDB
+              </h1>
+              <SearchForm btnVariant="light" />
+            </Col>
+          </Row>
+          <ToastNotification />
+        </Container>
+      </BackgroundContainer>
+    </div>
   );
 }

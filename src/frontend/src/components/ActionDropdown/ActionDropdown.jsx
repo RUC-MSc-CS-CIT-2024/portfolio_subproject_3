426e434402921445d94a8f4fd2703e8dafb0249b
idax6797 <idahay24@gmail.com>
2024-12-02 16:21:47 +0000

    feat: set up collapseable form and remove dropdown

--- src/frontend/src/components/ActionDropdown/ActionDropdown.jsx
+++ src/frontend/src/components/ActionDropdown/ActionDropdown.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react';
-import { Button, Form, Dropdown } from 'react-bootstrap';
+import { Button, Collapse, Form } from 'react-bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-export default function ActionDropdown({
-  title,
-  formFields,
-  handleSubmit,
-  buttonText,
-}) {
+export default function ActionDropdown({ title, formFields, handleSubmit }) {
+  const [open, setOpen] = useState(false);
   const [formData, setFormData] = useState(
     formFields.reduce((acc, field) => {
       acc[field.name] = field.defaultValue || '';
@@ -24,37 +21,46 @@ export default function ActionDropdown({
   };
 
   return (
-    <Dropdown>
-      <Dropdown.Toggle
+    <>
+      <Button
         variant="outline-dark"
-        id="dropdown-basic"
-        className="w-100 mb-2"
+        onClick={() => setOpen(!open)}
+        aria-controls="form-collapse"
+        aria-expanded={open}
+        className="w-100 mb-2 rounded"
       >
         {title}
-      </Dropdown.Toggle>
-      <Dropdown.Menu className="w-100">
-        <Form className="p-3">
-          {formFields.map((field) => (
-            <Form.Group controlId={field.name} key={field.name}>
-              <Form.Control
-                type={field.type}
-                placeholder={field.placeholder}
-                name={field.name}
-                value={formData[field.name]}
-                onChange={handleChange}
-                className="mb-2"
-              />
-            </Form.Group>
-          ))}
-          <Button
-            variant="outline-dark"
-            onClick={handleFormSubmit}
-            className="mt-2"
-          >
-            {buttonText}
-          </Button>
-        </Form>
-      </Dropdown.Menu>
-    </Dropdown>
+        {!open ? (
+          <i className="bi bi-chevron-down ms-1"></i>
+        ) : (
+          <i className="bi bi-chevron-up ms-1"></i>
+        )}
+      </Button>
+      <Collapse in={open}>
+        <div id="form-collapse" className="w-100">
+          <Form className="p-3">
+            {formFields.map((field) => (
+              <Form.Group controlId={field.name} key={field.name}>
+                <Form.Control
+                  type={field.type}
+                  placeholder={field.placeholder}
+                  name={field.name}
+                  value={formData[field.name]}
+                  onChange={handleChange}
+                  className="mb-2"
+                />
+              </Form.Group>
+            ))}
+            <Button
+              variant="outline-dark"
+              onClick={handleFormSubmit}
+              className="mt-2"
+            >
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </Collapse>
+    </>
   );
 }

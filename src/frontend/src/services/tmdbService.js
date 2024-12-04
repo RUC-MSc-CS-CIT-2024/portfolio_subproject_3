88dd1f3770943e6ac6390150571a79a30baf551f
Sofus Hilfling <sofushn@hotmail.dk>
2024-12-04 10:06:02 +0100

    feat: setup list fetching  (#69)
    - `ApiClient` - for simplifying api calls through a generic class
    - `tmdbService` with `getTMDBImage()` function for getting images
      - input the imdb id and an optional size 
    - use the `ImageSize` const from the tmdbService for a list of allowed
    sizes
    - add function to userService for fetching 
      - bookmark
      - completed
      - persons
    - async effect hook
    
    > [!WARNING]
    > This PR requires the latest changes in the backend

---
+++ src/frontend/src/services/tmdbService.js
@@ -0,0 +1,39 @@
+import { ApiClient } from '../utils/apiClient';
+
+const baseUrl = import.meta.env.VITE_TMDB_BASE_URL;
+const accessKey = import.meta.env.VITE_TMDB_READ_TOKEN;
+const client = new ApiClient(baseUrl, accessKey);
+
+export const ImageSize = Object.freeze({
+  VerySmall: 'w92', // Used on lists
+  Small: 'w185', // Used for cards
+  Normal: 'w500', // Used on the detailed pages
+  Original: 'original',
+});
+
+export async function getTMDBImage(imdb_id, size) {
+  size = size ?? ImageSize.Normal;
+  const resp = await client.Get(`find/${imdb_id}?external_source=imdb_id`);
+  if (!resp.ok) {
+    throw new Error('Network response was not ok');
+  }
+  const val = resp.value;
+  let path = '';
+  if (val.movie_results.length > 0) {
+    path = val.movie_results[0].poster_path;
+  } else if (val.tv_results.length > 0) {
+    path = val.tv_results[0].poster_path;
+  } else if (val.tv_episode_results.length > 0) {
+    path = val.tv_episode_results[0].still_path;
+  } else if (val.person_results.length > 0) {
+    path = val.person_results[0].profile_path;
+  } else {
+    throw new Error('No image found');
+  }
+
+  const imageUrlWithSize = new URL(
+    size + '/',
+    import.meta.env.VITE_TMDB_IMAGE_BASE_URL,
+  );
+  return new URL(path.substring(1), imageUrlWithSize).href;
+}

fc0a152c72384ad61caabbff993b7a9ce6d010b0
Sofus Hilfling <sofushn@hotmail.dk>
2024-12-04 10:23:34 +0100

    fix: import of removed dependencies (#73)

--- src/frontend/src/pages/index.js
+++ src/frontend/src/pages/index.js
@@ -1,7 +1,6 @@
 export { default as HomePage } from './HomePage';
 export { default as NotFoundPage } from './NotFoundPage';
 export { default as ProfilePage } from './ProfilePage';
-export { default as ProfileSettingsPage } from './ProfileSettingsPage';
 export { default as UserListsPage } from './UserListsPage';
 export { default as MediaOverviewPage } from './MediaOverviewPage';
 export { default as MediaDetailPage } from './MediaDetailPage';

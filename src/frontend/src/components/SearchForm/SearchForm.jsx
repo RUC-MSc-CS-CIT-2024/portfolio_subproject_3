6215ff832268f0189d8eb00ad564c01c6b2e079f
sofushn <sofushn@hotmail.dk>
2024-12-13 10:44:47 +0000

    fix: backgroup image height

--- src/frontend/src/components/SearchForm/SearchForm.jsx
+++ src/frontend/src/components/SearchForm/SearchForm.jsx
@@ -70,59 +70,57 @@ export default function SearchForm({ btnVariant = 'dark', onSearch }) {
   };
 
   return (
-    <>
-      <div className="search-form-container" ref={formRef}>
-        <Form className="d-flex my-3" onSubmit={handleSearch}>
-          <Form.Control
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-            size="sm"
-            name="query"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            onFocus={() => setShowDropdown(true)}
-            onBlur={() => setTimeout(() => setShowDropdown(false), 200)}
-          />
-          <Button variant={btnVariant} size="sm" type="submit">
-            <span className="d-flex gap-2">
-              <i className="bi bi-search"></i> Search
-            </span>
-          </Button>
-        </Form>
-        {showDropdown && searchHistory.length > 0 && (
-          <Dropdown show={showDropdown} className="w-100">
-            <Dropdown.Menu className="w-100">
-              {searchHistory.map((item, index) => (
-                <Dropdown.Item
-                  key={index}
-                  className="d-flex justify-content-between align-items-center"
-                >
-                  <span onMouseDown={() => setSearchQuery(item.searchText)}>
-                    {item.searchText}
-                  </span>
-                  <i
-                    className="bi bi-x text-danger"
-                    style={{ cursor: 'pointer' }}
-                    onMouseDown={(e) => e.preventDefault()}
-                    onClick={() => handleDelete(item.searchHistoryId)}
-                  />
-                </Dropdown.Item>
-              ))}
-              {hasMoreItems && (
-                <Dropdown.Item
+    <div className="search-form-container" ref={formRef}>
+      <Form className="d-flex my-3" onSubmit={handleSearch}>
+        <Form.Control
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+          size="sm"
+          name="query"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onFocus={() => setShowDropdown(true)}
+          onBlur={() => setTimeout(() => setShowDropdown(false), 200)}
+        />
+        <Button variant={btnVariant} size="sm" type="submit">
+          <span className="d-flex gap-2">
+            <i className="bi bi-search"></i> Search
+          </span>
+        </Button>
+      </Form>
+      {showDropdown && searchHistory.length > 0 && (
+        <Dropdown show={showDropdown} className="w-100">
+          <Dropdown.Menu className="w-100">
+            {searchHistory.map((item, index) => (
+              <Dropdown.Item
+                key={index}
+                className="d-flex justify-content-between align-items-center"
+              >
+                <span onMouseDown={() => setSearchQuery(item.searchText)}>
+                  {item.searchText}
+                </span>
+                <i
+                  className="bi bi-x text-danger"
+                  style={{ cursor: 'pointer' }}
                   onMouseDown={(e) => e.preventDefault()}
-                  onClick={handleNextPage}
-                  className="fw-bold"
-                >
-                  Load more...
-                </Dropdown.Item>
-              )}
-            </Dropdown.Menu>
-          </Dropdown>
-        )}
-      </div>
-    </>
+                  onClick={() => handleDelete(item.searchHistoryId)}
+                />
+              </Dropdown.Item>
+            ))}
+            {hasMoreItems && (
+              <Dropdown.Item
+                onMouseDown={(e) => e.preventDefault()}
+                onClick={handleNextPage}
+                className="fw-bold"
+              >
+                Load more...
+              </Dropdown.Item>
+            )}
+          </Dropdown.Menu>
+        </Dropdown>
+      )}
+    </div>
   );
 }

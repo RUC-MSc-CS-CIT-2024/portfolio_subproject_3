91002add1f6d5514cb133155b5b14ae317fb9ce3
sofushn <sofushn@hotmail.dk>
2024-12-15 22:34:14 +0000

    fix: titels not displaying

--- src/frontend/src/services/mediaService.js
+++ src/frontend/src/services/mediaService.js
@@ -25,11 +25,15 @@ export const fetchTitles = async (id, page, count) => {
 
   const path = `/api/media/${id}/titles`;
   try {
-    const response = await api.Get(path, queryParams);
-    if (!response.ok) {
+    const responseSize = await api.Get(path);
+    if (!responseSize.ok) {
       throw new Error(`Failed to fetch titles for media ID ${id}.`);
     }
-    return response.value;
+    const response = await api.Get(path, [
+      { key: 'page', value: 1 },
+      { key: 'count', value: responseSize.value.numberOfItems },
+    ]);
+    return response.value.items;
   } catch (error) {
     console.error('Failed to fetch media titles:', error);
     throw error;

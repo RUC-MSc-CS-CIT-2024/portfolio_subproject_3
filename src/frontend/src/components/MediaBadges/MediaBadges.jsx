1c44d494cc8b253fdaefac3e851806a80ba6414f
idax6797 <idahay24@gmail.com>
2024-12-16 09:30:49 +0100

    fix: set up fetcing all titles and display them in the mediaBadges remove on click

--- src/frontend/src/components/MediaBadges/MediaBadges.jsx
+++ src/frontend/src/components/MediaBadges/MediaBadges.jsx
@@ -1,26 +1,12 @@
-import { useNavigate } from 'react-router';
 import { Badge, Row, Col } from 'react-bootstrap';
 import './MediaBadges.css';
 
 export default function MediaBadges({ badges }) {
-  const navigate = useNavigate();
-
-  const handleBadgeClick = (badge) => {
-    if (badge) {
-      navigate(`/${badge.toLowerCase()}`);
-    }
-  };
   return (
     <Row>
       {badges.map((badge, index) => (
         <Col key={index} xs="auto" className="mb-2">
-          <Badge
-            bg="secondary"
-            onClick={() => handleBadgeClick(badge)}
-            className="pointer"
-          >
-            {badge}
-          </Badge>
+          <Badge bg="secondary">{badge}</Badge>
         </Col>
       ))}
     </Row>

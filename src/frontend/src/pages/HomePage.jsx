9d6445ffbbd3efd42de383c04e48c7dee4427f86
idax6797 <idahay24@gmail.com>
2024-12-17 12:14:41 +0100

    fix: remove background image

--- src/frontend/src/pages/HomePage.jsx
+++ src/frontend/src/pages/HomePage.jsx
@@ -1,31 +1,23 @@
 import { Container, Col, Row } from 'react-bootstrap';
-import {
-  BackgroundContainer,
-  SearchForm,
-  ToastNotification,
-} from '@/components';
+import { SearchForm, ToastNotification } from '@/components';
 import { useSearch } from '@/hooks';
 
 export default function HomePage() {
   const { searchWithNavigation } = useSearch();
   return (
-    <div>
-      <BackgroundContainer>
-        <Container>
-          <Row>
-            <Col>
-              <h1 className="text-center my-5 text-light">
-                Welcome to MovieDB
-              </h1>
-              <SearchForm
-                btnVariant="light"
-                onSearch={(e) => searchWithNavigation(e)}
-              />
-            </Col>
-          </Row>
-          <ToastNotification />
-        </Container>
-      </BackgroundContainer>
+    <div className="homepage">
+      <Container className="h-100">
+        <Row className="justify-content-center align-items-center h-100">
+          <Col xs={12} md={8} lg={6}>
+            <h1 className="text-center my-5">Welcome to MovieDB</h1>
+            <SearchForm
+              btnVariant="dark"
+              onSearch={(e) => searchWithNavigation(e)}
+            />
+          </Col>
+        </Row>
+        <ToastNotification />
+      </Container>
     </div>
   );
 }
